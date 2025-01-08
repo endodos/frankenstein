f827ee192bb8cfd5cfc928f0034b267c57285e89
laa0304 <endo.rodriguez@externos-co.cencosud.com>
2025-01-08 17:52:58 -0500

    feat: creacion de la estructura del proyecto

---
+++ frankenstein/src/test/java/com/endorodrigo/frankenstein/FrankensteinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endorodrigo.frankenstein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankensteinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
